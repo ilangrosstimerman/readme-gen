d02cba426b6c91eab49bd9b9c11b0cf867c1da63
ilantimerman <ilan.tmrmn@gmail.com>
2022-02-20 14:12:05 -0800

    add trimAnswer as filter for input

--- index.js
+++ index.js
@@ -4,46 +4,59 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
 const questions = [
-        {
-          type: 'input',
-          name: 'title',
-          message: 'What is the title of the project?',
-          validate: noNewlineChar,
-        },
-        {
-          type: 'input',
-          name: 'license',
-          message: 'What license does this project fall under?',
-          choices: [
-            'Apache License 2.0',
-            'GNU GPLv3',
-            'MIT License',
-            'ISC License',
-            ],
-        },
-        {
-          type: 'input',
-          name: 'contributing',
-          message: 'How many contributors contributed to this project?',
-        },
-        {
-          type: 'input',
-          name: 'test',
-          message: 'What are some tests for the project?',
-        },
-        {
-          type: 'input',
-          name: 'userName',
-          message: 'What is your GitHub username?',
-          validate: noNewlineChar,
-        },
-        {
-          type: 'input',
-          name: 'email',
-          message: 'What is your email address?',
-          validate: noNewlineChar,
-        },
-      ]
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of this project?',
+        validate: noNewlineChar,
+        filter: trimAnswer,
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Describe this project:',
+        filter: trimAnswer,
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install this project?',
+        filter: trimAnswer,
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'How do you use this project?',
+        filter: trimAnswer,
+      },
+      {
+        type: 'list',
+        type: 'input',
+        name: 'contributing',
+        message: 'How may others contribute to this project?',
+        filter: trimAnswer,
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'What are some tests for this project?',
+        filter: trimAnswer,
+      },
+      {
+        type: 'input',
+        name: 'userName',
+        message: 'What is your GitHub username?',
+        validate: noNewlineChar,
+        filter: trimAnswer,
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email?',
+        validate: noNewlineChar,
+        filter: trimAnswer,
+      },
+    ];
 
 //function to validate input
 function noNewlineChar(input) {
@@ -55,6 +68,15 @@ function noNewlineChar(input) {
       }
     });
   }
+
+//function to trim answers
+
+  function trimAnswer(input) {
+    return new Promise((resolve, reject) => {
+      resolve(input.trim());
+    });
+  }
+  
   
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
@@ -65,16 +87,13 @@ function writeToFile(fileName, data) {
     }
 
 // TODO: Create a function to initialize app
-function init() {
 
-        inquirer
-        .prompt(questions)
-        .then(answers => {
-          console.log(answers);
-          const markDown = generateMarkdown(answers);
-          writeToFile('samplereadme.md', markDown);
-        });
-      }
+async function init() {
+    const answers = await inquirer.prompt(questions);
+    const markdown = generateMarkdown(answers);
+    writeToFile('genSampleReadme.md', markdown);
+}
+      
 
 // Function call to initialize app
 init();
