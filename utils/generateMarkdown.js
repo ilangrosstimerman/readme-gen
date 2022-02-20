1e5720d6e53876d291882027c8e091e3c389a87e
ilantimerman <ilan.tmrmn@gmail.com>
2022-02-20 14:27:47 -0800

    add get section fn to installation:

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -31,7 +31,7 @@ ${data.description}
 - [Tests](#tests)
 - [Questions](#questions)
 ## Installation
-${data.installation}
+${getSection('Installation', data.installation)}
 ## Usage
 ${data.usage}
 ## License
