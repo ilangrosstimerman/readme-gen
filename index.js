3bb65d8583d2406b2f952d2afb5af6dfbdd02e92
ilantimerman <ilan.tmrmn@gmail.com>
2022-02-19 16:22:44 -0800

    require file system module

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
 const inquirer = require('inquirer');
 const generateMarkdown = require('utils/generateMarkdown.js')
 // TODO: Create an array of questions for user input
@@ -8,7 +9,15 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+        inquirer
+        .prompt(questions)
+        .then(answers => {
+          console.log(answers);
+        });
+      }
 
 // Function call to initialize app
 init();
+
